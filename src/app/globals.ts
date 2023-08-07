4400f5a8e565e6ec0bfd83f6ef43c6925c3c531c
cosmoDev1 <admin@cosmoapp.org>
2023-08-07 10:53:24 -0500

    commit

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -53,7 +53,7 @@ export interface Animal {
 
 @Injectable({ providedIn: 'root' })
 export class Globals {
-    public webserviceUrl: string = "https://cosmoapp.org/webservices/api/values?testSource=true";
+    public webserviceUrl: string = "https://cosmoapp.org/webservices/api/values?testSource=false";
     //public webserviceUrl: string = "https://localhost:44307/api/values?testSource=true";
     public rescueName: string = "Bernie Test Rescue";
     public states: any = [
