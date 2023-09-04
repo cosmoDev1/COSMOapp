fa0b3a1ab7e4aa4777453ebd199be203f6167dc8
cosmoDev1 <admin@cosmoapp.org>
2023-09-03 21:17:08 -0500

    registration step 2 - 3/3

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -22,9 +22,15 @@ export class RegisterComponent implements OnInit {
       rescuedata = { rescueName: "", rescueAddress: "", rescueCity: "", rescueState: "", rescueZip: "", rescuePhone: "", rescueEmail: "", contactName: "", contactEmail: "", contactPhone: "",
             species: "1", gender: "1", size: "1", age: "1", applicationType: "1" };
 
+      fosterdata = { fosterName: "", fosterAddress: "", fosterCity: "", fosterState: "TX", fosterZip: "", fosterPhone: "", fosterEmail: "", fosterDL: "", fosterHome: "1", fosterOwnership: "1",
+            fosterLandlord: "1", fosterYard: "1", fosterFence: "", exerciseMethod: "", addressDuration: "", fosterSpecies: "1", fosterPetGender: "1", fosterPetSize: "1", petExperience: "",
+            ownedBefore: "1", breedType: "", fosteredBefore: "1", fosterExperience: "", firstPet: "", firstYears: "", firstGender: "1", spayedNeutered1: false, secondPet: "", secondYears: "",
+            twoGender: "1", spayedNeutered2: false, thirdPet: "", thirdYears: "", thirdGender: "1", spayedNeutered3: false, vetName: "", vetPhone: "", hoursAlone: "", dailyOccupancy: "",
+            nightlyOccupancy: "", crateTrained: "1", adultOne: "", adultOneAge: "", adultOneRelation: "", adultTwo: "", adultTwoAge: "", adultTwoRelation: "",
+            childOne: "", childOneAge: "", childOneRelation: "", childTwo: "", childTwoAge: "", childTwoRelation: "", anyoneAllergic: "1", childrenHome: "1", accommodations: "",
+            referenceOne: "", firstPhone: "", referenceTwo: "", secondPhone: "", referenceThree: "", thirdPhone: "" }
 
       constructor(private http: HttpClient, public global: Globals, private _formBuilder: FormBuilder) {  }
-
 
       ngOnInit() { console.log('registering') }
 
