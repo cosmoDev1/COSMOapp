c9bfe90d3ff7d6bba851dc3ac51692fcddb642f0
cosmoDev1 <admin@cosmoapp.org>
2023-09-03 20:58:39 -0500

    registration step 2 - 2/3

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -12,14 +12,29 @@ import { Globals } from '../globals';
 export class RegisterComponent implements OnInit {
       title = 'COSMO app - Register';
       isOptional = false;
-      formdata = { typeAccount: "1"}
+      entireFormEnabled = 'false';
+
+      formdata = { typeAccount: "1" }
+
+      shelterdata = { shelterName: "", shelterAddress: "", shelterCity: "", shelterState: "TX", shelterZip: "", shelterPhone: "", adminName: "", adminPhone: "", adminEmail: "",
+            coordinatorName: "", coordinatorPhone: "", coordinatorEmail: "" };
+
+      rescuedata = { rescueName: "", rescueAddress: "", rescueCity: "", rescueState: "", rescueZip: "", rescuePhone: "", rescueEmail: "", contactName: "", contactEmail: "", contactPhone: "",
+            species: "1", gender: "1", size: "1", age: "1", applicationType: "1" };
+
 
       constructor(private http: HttpClient, public global: Globals, private _formBuilder: FormBuilder) {  }
 
 
-      ngOnInit() {
-            console.log('registering')
+      ngOnInit() { console.log('registering') }
 
+      numberOnlyEvent(event: any): void {
+            const charCode = (event.which) ? event.which : event.keyCode;
+
+            if (charCode > 31 && (charCode < 48 || charCode > 57) && (charCode < 96 || charCode > 105) && charCode !== 110 && charCode !== 190) {
+                  // Prevent non-numeric characters
+                  event.preventDefault();
+            }
       }
 
 }
