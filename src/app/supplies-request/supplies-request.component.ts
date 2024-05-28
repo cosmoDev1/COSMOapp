efbfa1ae7f3c1f6067b234b24512239661d36386
Jackie Amador Arrazola <jackie.arrazola15@gmail.com>
2024-05-27 19:30:52 -0700

    new changes console log working, inputed text doesnt want to work and the mat error doesnt want to work either there are no errors though just need help with that - Jackie

--- src/app/supplies-request/supplies-request.component.ts
+++ src/app/supplies-request/supplies-request.component.ts
@@ -7,12 +7,23 @@ import { MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
   styleUrls: ['./supplies-request.component.css']
 })
 export class SuppliesRequestComponent {
-    dialogRef: any;
 
+    supplyData = { RadioDescription: "", Quantity: "", TextDescription: "", }
 
+    entireFormEnabled: boolean = false;
+    constructor(public dialogRef: MatDialogRef<SuppliesRequestComponent>) { }
 
+    submitDialog(supplyInfo: any) { 
+     var dataToSend = { RadioDescription: this.supplyData.RadioDescription.toLocaleString(), Quantity: this.supplyData.Quantity, TextDescription: this.supplyData.TextDescription }
 
+    console.log(supplyInfo)
 
+    if (supplyInfo.invalid) { return; }
+
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.disableClose = true;
+    console.log(dataToSend)
+    }
 
 
     closeDialog() {
