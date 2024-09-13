c6ac55962ea83dadfdd447df1ad7316052b205c0
javier.h7548@gmail.com <javier.h7548@gmail.com>
2024-09-12 20:39:12 -0500

    pre

--- src/app/bio-dialog/bio-dialog.component.ts
+++ src/app/bio-dialog/bio-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ApplicationInitStatus, Component } from '@angular/core';
 import { MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
 
 @Component({
@@ -10,20 +10,31 @@ export class BioDialogComponent {
 
     bioData = { SelectedDate: "", BehaveriolDescription: "" };
 
-    entireFormEnabled: boolean = false;
-constructor(public dialogRef: MatDialogRef<BioDialogComponent>) { }
+
+    entireFormEnabled: boolean = true;
+    constructor(public dialogRef: MatDialogRef<BioDialogComponent>) { }
 
     submitDialog(bioInfo: any) {
         var dataToSend = { SelectedDate: this.bioData.SelectedDate.toLocaleString(), BehaveriolDescription: this.bioData.BehaveriolDescription }
 
-        console.log(bioInfo)
+        console.log(this.bioData)
 
         if (bioInfo.invalid) { return; }
 
         const dialogConfig = new MatDialogConfig(); 
         dialogConfig.disableClose = true;
         console.log(dataToSend)
+        //here we send that info to the database using an httpclient
 
+
+        //loosely typed language
+        //strongly typed language - strict mode
+
+        //SQL         C#               HTML CSS javascript typescript Angular (typescript framework)  React
+        database < -> server side                    <-> client side
+                      //API                               //http client
+                      //webservices -> endpoints
+                      //sql clients
 
     }
 
