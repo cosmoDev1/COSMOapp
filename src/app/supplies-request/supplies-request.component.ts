c079b3f228a18db15c746056286d398afb72f8d9
niel ner <danielpagar18@gmail.com>
2024-10-21 20:37:15 -0500

    supplies request call webservice

--- src/app/supplies-request/supplies-request.component.ts
+++ src/app/supplies-request/supplies-request.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef, MatDialogConfig } from '@angular/material/dialog';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-supplies-request',
@@ -11,18 +12,29 @@ export class SuppliesRequestComponent {
     supplyData = { radioDescription: "1", quantity: "", textDescription: "", }
 
     entireFormEnabled: boolean = false;
-    constructor(public dialogRef: MatDialogRef<SuppliesRequestComponent>) { }
+    constructor(public dialogRef: MatDialogRef<SuppliesRequestComponent>, public http: HttpClient) { }
 
     submitDialog(supplyInfo: any) {
         var dataToSend = { radioDescription: this.supplyData.radioDescription, quantity: this.supplyData.quantity, textDescription: this.supplyData.textDescription }
+
+
+        this.http.post('http://localhost:5000/api/suppliesrequest', dataToSend).subscribe((res: any) => {
+
+            console.log('everything went ok')
+            console.log(res);
+
+        }, (err: any) => {
+            //this section only happens if clientside -> serverside fails
+            console.log('something horrible happened')
+        })
 
         console.log(supplyInfo)
 
         if (supplyInfo.invalid) { return; }
 
-        const dialogConfig = new MatDialogConfig();
-        dialogConfig.disableClose = true;
-        console.log(dataToSend)
+        //const dialogConfig = new MatDialogConfig();
+        //dialogConfig.disableClose = true;
+        ///console.log(dataToSend)
     }
 
 
